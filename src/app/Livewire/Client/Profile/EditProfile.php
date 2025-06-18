acc3104a163137f918d1883fc9dd71fe5005351c
Bagus Kusuma Putra <baguskp@gmail.com>
2025-06-18 16:08:22 +0700

    feat(Profile): add sell price field to profile editing
    Introduce a new field 'profileSellPrice' to allow setting a separate selling price for profiles. This enables more flexible pricing strategies beyond just the base price.

--- src/app/Livewire/Client/Profile/EditProfile.php
+++ src/app/Livewire/Client/Profile/EditProfile.php
@@ -18,6 +18,7 @@ class EditProfile extends Component
     public $profileName;
     public $profileDescription;
     public $profilePrice = 0;
+    public $profileSellPrice = 0;
     public $profileUptimeLimit = 0;
     public $profileValidity = 0;
     public $profileMaxDownload = 0;
@@ -33,6 +34,7 @@ class EditProfile extends Component
             'profileName'           =>  'required|min:5|max:50',
             'profileDescription'    =>  'max:50',
             'profilePrice'          =>  'min:0|numeric',
+            'profileSellPrice'      =>  'min:0|numeric',
             'profileUptimeLimit'    =>  'min:0|numeric',
             'profileValidity'       =>  'min:0|numeric',
             'profileMaxDownload'    =>  'min:0|numeric',
@@ -48,6 +50,7 @@ class EditProfile extends Component
             'name'          =>  $this->profileName,
             'description'   =>  $this->profileDescription,
             'price'         =>  $this->profilePrice,
+            'sell_price'    =>  $this->profileSellPrice,
             'uptime_limit'  =>  $this->profileUptimeLimit,
             'data_limit'    =>  $this->profileDataLimit,
             'max_download'  =>  $this->profileMaxDownload,
